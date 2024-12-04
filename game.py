57abd86bb6449f014e209c9f424ee96b097aa1d8
Hazel Jeong <hjeong29@my.bcit.ca>
2024-12-04 00:08:55 -0800

    Add doctest to dishes

--- game.py
+++ game.py
@@ -36,6 +36,16 @@ def dishes() -> dict:
 
     :postcondition: return a dictionary containing the dish options
     :return: the dish options in a dictionary
+
+    >>> dishes() # doctest: +NORMALIZE_WHITESPACE
+    {'Galbijjim': {'Difficulty': 5, 'Vegetables': ('Carrots', 'Potatoes', 'Onions', 'Garlic'), 'Meat': 'Beef'},
+    'Jeyuk': {'Difficulty': 3, 'Vegetables': ('Carrots', 'Onions', 'Garlic'), 'Meat': 'Pork'},
+    'Dakgalbi': {'Difficulty': 2, 'Vegetables': ('Cabbages', 'Potatoes', 'Onions', 'Garlic'),
+                 'Meat': 'Chicken'},
+    'Kongbul': {'Difficulty': 3, 'Vegetables': ('Bean Sprouts', 'Onions', 'Garlic'), 'Meat': 'Pork'},
+    'Jjimdak': {'Difficulty': 2, 'Vegetables': ('Carrots', 'Potatoes', 'Onions', 'Garlic'), 'Meat': 'Chicken'},
+    'Samgyeopsal': {'Difficulty': 1, 'Vegetables': ('Lettuce', 'Garlic'), 'Meat': 'Pork'},
+    'Bulgogi': {'Difficulty': 4, 'Vegetables': ('Carrots', 'Onions', 'Garlic'), 'Meat': 'Beef'}}
     """
     return {"Galbijjim": {"Difficulty": 5, "Vegetables": ("Carrots", "Potatoes", "Onions", "Garlic"), "Meat": "Beef"},
             "Jeyuk": {"Difficulty": 3, "Vegetables": ("Carrots", "Onions", "Garlic"), "Meat": "Pork"},
