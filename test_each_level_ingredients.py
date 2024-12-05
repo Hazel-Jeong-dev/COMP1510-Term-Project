e98ec87e3df14e8db92c80df84ac435241c253d7
Hazel Jeong <hjeong29@my.bcit.ca>
2024-12-04 16:25:47 -0800

    Add unit tests for each_level_ingredients

---
+++ test_each_level_ingredients.py
@@ -0,0 +1,124 @@
+from unittest import TestCase
+
+import game
+
+
+class TestEachLevelIngredients(TestCase):
+
+    def test_each_level_ingredients_return_a_dictionary(self):
+        dishes = {
+            "Galbijjim": {"Difficulty": 5, "Vegetables": ("Carrots", "Potatoes", "Onions", "Garlic"), "Meat": "Beef"},
+            "Jeyuk": {"Difficulty": 3, "Vegetables": ("Carrots", "Onions", "Garlic"), "Meat": "Pork"},
+            "Dakgalbi": {"Difficulty": 2, "Vegetables": ("Cabbages", "Potatoes", "Onions", "Garlic"),
+                         "Meat": "Chicken"},
+            "Kongbul": {"Difficulty": 3, "Vegetables": ("Bean Sprouts", "Onions", "Garlic"), "Meat": "Pork"},
+            "Jjimdak": {"Difficulty": 2, "Vegetables": ("Carrots", "Potatoes", "Onions", "Garlic"), "Meat": "Chicken"},
+            "Samgyeopsal": {"Difficulty": 1, "Vegetables": ("Lettuce", "Garlic"), "Meat": "Pork"},
+            "Bulgogi": {"Difficulty": 4, "Vegetables": ("Carrots", "Onions", "Garlic"), "Meat": "Beef"}}
+        dish = "Galbijjim"
+        self.assertIsInstance(game.each_level_ingredients(dishes, dish), dict)
+
+    def test_each_level_ingredients_galbijjim(self):
+        dishes = {
+            "Galbijjim": {"Difficulty": 5, "Vegetables": ("Carrots", "Potatoes", "Onions", "Garlic"), "Meat": "Beef"},
+            "Jeyuk": {"Difficulty": 3, "Vegetables": ("Carrots", "Onions", "Garlic"), "Meat": "Pork"},
+            "Dakgalbi": {"Difficulty": 2, "Vegetables": ("Cabbages", "Potatoes", "Onions", "Garlic"),
+                         "Meat": "Chicken"},
+            "Kongbul": {"Difficulty": 3, "Vegetables": ("Bean Sprouts", "Onions", "Garlic"), "Meat": "Pork"},
+            "Jjimdak": {"Difficulty": 2, "Vegetables": ("Carrots", "Potatoes", "Onions", "Garlic"), "Meat": "Chicken"},
+            "Samgyeopsal": {"Difficulty": 1, "Vegetables": ("Lettuce", "Garlic"), "Meat": "Pork"},
+            "Bulgogi": {"Difficulty": 4, "Vegetables": ("Carrots", "Onions", "Garlic"), "Meat": "Beef"}}
+        dish = "Galbijjim"
+        actual = game.each_level_ingredients(dishes, dish)
+        expected = {1: ("Carrots", "Potatoes", "Onions", "Garlic"), 2: 'Beef', 3: 'Galbijjim'}
+        self.assertEqual(actual, expected)
+
+    def test_each_level_ingredients_jeyuk(self):
+        dishes = {
+            "Galbijjim": {"Difficulty": 5, "Vegetables": ("Carrots", "Potatoes", "Onions", "Garlic"), "Meat": "Beef"},
+            "Jeyuk": {"Difficulty": 3, "Vegetables": ("Carrots", "Onions", "Garlic"), "Meat": "Pork"},
+            "Dakgalbi": {"Difficulty": 2, "Vegetables": ("Cabbages", "Potatoes", "Onions", "Garlic"),
+                         "Meat": "Chicken"},
+            "Kongbul": {"Difficulty": 3, "Vegetables": ("Bean Sprouts", "Onions", "Garlic"), "Meat": "Pork"},
+            "Jjimdak": {"Difficulty": 2, "Vegetables": ("Carrots", "Potatoes", "Onions", "Garlic"), "Meat": "Chicken"},
+            "Samgyeopsal": {"Difficulty": 1, "Vegetables": ("Lettuce", "Garlic"), "Meat": "Pork"},
+            "Bulgogi": {"Difficulty": 4, "Vegetables": ("Carrots", "Onions", "Garlic"), "Meat": "Beef"}}
+        dish = "Jeyuk"
+        actual = game.each_level_ingredients(dishes, dish)
+        expected = {1: ("Carrots", "Onions", "Garlic"), 2: 'Pork', 3: 'Jeyuk'}
+        self.assertEqual(actual, expected)
+
+    def test_each_level_ingredients_dakgalbi(self):
+        dishes = {
+            "Galbijjim": {"Difficulty": 5, "Vegetables": ("Carrots", "Potatoes", "Onions", "Garlic"), "Meat": "Beef"},
+            "Jeyuk": {"Difficulty": 3, "Vegetables": ("Carrots", "Onions", "Garlic"), "Meat": "Pork"},
+            "Dakgalbi": {"Difficulty": 2, "Vegetables": ("Cabbages", "Potatoes", "Onions", "Garlic"),
+                         "Meat": "Chicken"},
+            "Kongbul": {"Difficulty": 3, "Vegetables": ("Bean Sprouts", "Onions", "Garlic"), "Meat": "Pork"},
+            "Jjimdak": {"Difficulty": 2, "Vegetables": ("Carrots", "Potatoes", "Onions", "Garlic"), "Meat": "Chicken"},
+            "Samgyeopsal": {"Difficulty": 1, "Vegetables": ("Lettuce", "Garlic"), "Meat": "Pork"},
+            "Bulgogi": {"Difficulty": 4, "Vegetables": ("Carrots", "Onions", "Garlic"), "Meat": "Beef"}}
+        dish = "Dakgalbi"
+        actual = game.each_level_ingredients(dishes, dish)
+        expected = {1: ("Cabbages", "Potatoes", "Onions", "Garlic"), 2: 'Chicken', 3: 'Dakgalbi'}
+        self.assertEqual(actual, expected)
+
+    def test_each_level_ingredients_kongbul(self):
+        dishes = {
+            "Galbijjim": {"Difficulty": 5, "Vegetables": ("Carrots", "Potatoes", "Onions", "Garlic"), "Meat": "Beef"},
+            "Jeyuk": {"Difficulty": 3, "Vegetables": ("Carrots", "Onions", "Garlic"), "Meat": "Pork"},
+            "Dakgalbi": {"Difficulty": 2, "Vegetables": ("Cabbages", "Potatoes", "Onions", "Garlic"),
+                         "Meat": "Chicken"},
+            "Kongbul": {"Difficulty": 3, "Vegetables": ("Bean Sprouts", "Onions", "Garlic"), "Meat": "Pork"},
+            "Jjimdak": {"Difficulty": 2, "Vegetables": ("Carrots", "Potatoes", "Onions", "Garlic"), "Meat": "Chicken"},
+            "Samgyeopsal": {"Difficulty": 1, "Vegetables": ("Lettuce", "Garlic"), "Meat": "Pork"},
+            "Bulgogi": {"Difficulty": 4, "Vegetables": ("Carrots", "Onions", "Garlic"), "Meat": "Beef"}}
+        dish = "Kongbul"
+        actual = game.each_level_ingredients(dishes, dish)
+        expected = {1: ("Bean Sprouts", "Onions", "Garlic"), 2: 'Pork', 3: 'Kongbul'}
+        self.assertEqual(actual, expected)
+
+    def test_each_level_ingredients_jjimdak(self):
+        dishes = {
+            "Galbijjim": {"Difficulty": 5, "Vegetables": ("Carrots", "Potatoes", "Onions", "Garlic"), "Meat": "Beef"},
+            "Jeyuk": {"Difficulty": 3, "Vegetables": ("Carrots", "Onions", "Garlic"), "Meat": "Pork"},
+            "Dakgalbi": {"Difficulty": 2, "Vegetables": ("Cabbages", "Potatoes", "Onions", "Garlic"),
+                         "Meat": "Chicken"},
+            "Kongbul": {"Difficulty": 3, "Vegetables": ("Bean Sprouts", "Onions", "Garlic"), "Meat": "Pork"},
+            "Jjimdak": {"Difficulty": 2, "Vegetables": ("Carrots", "Potatoes", "Onions", "Garlic"), "Meat": "Chicken"},
+            "Samgyeopsal": {"Difficulty": 1, "Vegetables": ("Lettuce", "Garlic"), "Meat": "Pork"},
+            "Bulgogi": {"Difficulty": 4, "Vegetables": ("Carrots", "Onions", "Garlic"), "Meat": "Beef"}}
+        dish = "Jjimdak"
+        actual = game.each_level_ingredients(dishes, dish)
+        expected = {1: ("Carrots", "Potatoes", "Onions", "Garlic"), 2: 'Chicken', 3: 'Jjimdak'}
+        self.assertEqual(actual, expected)
+
+    def test_each_level_ingredients_samgyeopsal(self):
+        dishes = {
+            "Galbijjim": {"Difficulty": 5, "Vegetables": ("Carrots", "Potatoes", "Onions", "Garlic"), "Meat": "Beef"},
+            "Jeyuk": {"Difficulty": 3, "Vegetables": ("Carrots", "Onions", "Garlic"), "Meat": "Pork"},
+            "Dakgalbi": {"Difficulty": 2, "Vegetables": ("Cabbages", "Potatoes", "Onions", "Garlic"),
+                         "Meat": "Chicken"},
+            "Kongbul": {"Difficulty": 3, "Vegetables": ("Bean Sprouts", "Onions", "Garlic"), "Meat": "Pork"},
+            "Jjimdak": {"Difficulty": 2, "Vegetables": ("Carrots", "Potatoes", "Onions", "Garlic"), "Meat": "Chicken"},
+            "Samgyeopsal": {"Difficulty": 1, "Vegetables": ("Lettuce", "Garlic"), "Meat": "Pork"},
+            "Bulgogi": {"Difficulty": 4, "Vegetables": ("Carrots", "Onions", "Garlic"), "Meat": "Beef"}}
+        dish = "Samgyeopsal"
+        actual = game.each_level_ingredients(dishes, dish)
+        expected = {1: ("Lettuce", "Garlic"), 2: 'Pork', 3: 'Samgyeopsal'}
+        self.assertEqual(actual, expected)
+
+    def test_each_level_ingredients_bulgogi(self):
+        dishes = {
+            "Galbijjim": {"Difficulty": 5, "Vegetables": ("Carrots", "Potatoes", "Onions", "Garlic"), "Meat": "Beef"},
+            "Jeyuk": {"Difficulty": 3, "Vegetables": ("Carrots", "Onions", "Garlic"), "Meat": "Pork"},
+            "Dakgalbi": {"Difficulty": 2, "Vegetables": ("Cabbages", "Potatoes", "Onions", "Garlic"),
+                         "Meat": "Chicken"},
+            "Kongbul": {"Difficulty": 3, "Vegetables": ("Bean Sprouts", "Onions", "Garlic"), "Meat": "Pork"},
+            "Jjimdak": {"Difficulty": 2, "Vegetables": ("Carrots", "Potatoes", "Onions", "Garlic"), "Meat": "Chicken"},
+            "Samgyeopsal": {"Difficulty": 1, "Vegetables": ("Lettuce", "Garlic"), "Meat": "Pork"},
+            "Bulgogi": {"Difficulty": 4, "Vegetables": ("Carrots", "Onions", "Garlic"), "Meat": "Beef"}}
+        dish = "Bulgogi"
+        actual = game.each_level_ingredients(dishes, dish)
+        expected = {1: ("Carrots", "Onions", "Garlic"), 2: 'Beef', 3: 'Bulgogi'}
+        self.assertEqual(actual, expected)
